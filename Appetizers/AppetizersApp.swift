e1cfbb9da08457bf250ad8de714de3a1fd50cdea
sbibla <saar.bibla@gmail.com>
2024-04-17 09:40:40 -0700

    Initial Commit

---
+++ Appetizers/AppetizersApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizersApp.swift
+//  Appetizers
+//
+//  Created by Saar Bibla on 4/17/24.
+//
+
+import SwiftUI
+
+@main
+struct AppetizersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
